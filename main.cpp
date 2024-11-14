c1fdc5935e45de9d72778d176077a3a8a41432f8
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-11-14 09:19:00 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int bdisp, bpiano, h;
+    cout<<"Quanti blocchi ci sono?"<<endl;
+    cin>>bdisp;
+    bpiano=1;
+    h=0;
+    while(bdisp>=bpiano)
+    {
+        bdisp=bdisp-bpiano;
+        bpiano=bpiano+1;
+        h=h+1;
+    }
+    cout<<"L'altezza è "<<h;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
